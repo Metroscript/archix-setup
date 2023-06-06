f982e8da4d4960cc29fab4b2ff9e63193bb6c2b5
Metroscript <genericburner39@gmail.com>
2023-06-06 11:11:01 +1000

    Added changes for compliance with the GPL-3.0 license

--- setup-scripts/install-archlinux-support.sh
+++ setup-scripts/install-archlinux-support.sh
@@ -3,6 +3,8 @@
 # version: 1.1.6
 # shellcheck disable=SC2064
 
+# Original script by https://github.com/Lancia-Greggori
+
 # Modified by https://github.com/Metroscript to remove support for the now defunct [community] repo
 
 PROGRAM_NAME="$(basename "$0")"
