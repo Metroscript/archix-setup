582312aacc732ae0efc9cfd26052e909da1c801b
Metroscript <genericburner39@gmail.com>
2023-06-08 09:33:09 +1000

    Readded support for KDE Plasma

--- setup-scripts/packages.sh
+++ setup-scripts/packages.sh
@@ -26,10 +26,8 @@ elif grep -E "Integrated Graphics Controller" <<< $gpu || grep -E "Intel Corpora
 fi
 sudo pacman -Syu --needed --noconfirm vkd3d lib32-vkd3d
 
-    #Basic packages
-   #Make dirs
-#mkdir
-sudo pacman -Syu --needed --noconfirm linux-{headers,lts{,-headers}} pipewire{,-{audio,jack,pulse,alsa,v4l2}} wireplumber man-db alacritty wayland xorg-xwayland smartmontools v4l2loopback-dkms pkgfile gst-plugin-pipewire obs-studio gnu-free-fonts noto-fonts ttf-{jetbrains-mono-nerd,hack-nerd,ubuntu-nerd,noto-nerd} btop cups{,-pk-helper,-pdf} gutenprint foomatic-db-{engine,ppds,gutenprint-ppds} libsecret python-{mutagen,pysmbc} simple-scan yt-dlp ffmpeg atomicparsley firewalld fuse neofetch arj binutils bzip2 cpio gzip l{hasa,rzip,z{4,ip,op}} p7zip tar un{rar,zip,arj,ace} xz zip zstd squashfs-tools mpv fd bat lf lsd fortune-mod ponysay jre{-openjdk,11-openjdk,8-openjdk} rust libreoffice-fresh{,-en-gb} hunspell{,-en_au} coin-or-mp beanshell mariadb-libs postgresql-libs pstoedit sane gimp gparted gnome-{font-viewer,boxes} mythes-en lib{paper,wpg,pulse,mythes,32-{gnutls,libpulse,alsa-{lib,plugins},pipewire{,-jack,-v4l2},mesa}} keepassxc kdenlive bigsh0t dvgrab mediainfo noise-suppression-for-voice open{cv,timelineio} recordmydesktop rhythmbox lollypop gst-{libav,plugins-{base,good}} cmus krita okular apparmor deluge-gtk phonon-qt5-gstreamer imagemagick djvulibre ghostscript lib{heif,jxl,raw,rsvg,webp,wmf,xml2,zip} ocl-icd open{exr,jpeg2} materia-gtk-theme wget jq
+#Basic packages
+sudo pacman -Syu --needed --noconfirm linux-{headers,lts{,-headers}} pipewire{,-{audio,jack,pulse,alsa,v4l2}} wireplumber man-db wayland xorg-xwayland smartmontools v4l2loopback-dkms pkgfile gst-plugin-pipewire gnu-free-fonts noto-fonts ttf-{jetbrains-mono-nerd,hack-nerd,ubuntu-nerd,noto-nerd} cups{,-pk-helper,-pdf} gutenprint foomatic-db-{engine,ppds,gutenprint-ppds} libsecret python-{mutagen,pysmbc} yt-dlp ffmpeg atomicparsley firewalld fuse neofetch arj binutils bzip2 cpio gzip l{hasa,rzip,z{4,ip,op}} p7zip tar un{rar,zip,arj,ace} xz zip zstd squashfs-tools fd bat lsd fortune-mod ponysay jre{-openjdk,11-openjdk,8-openjdk} rust libreoffice-fresh{,-en-gb} hunspell{,-en_au} coin-or-mp beanshell mariadb-libs postgresql-libs pstoedit sane gimp mythes-en lib{paper,wpg,pulse,mythes,32-{gnutls,libpulse,alsa-{lib,plugins},pipewire{,-jack,-v4l2},mesa}} keepassxc gst-{libav,plugins-{base,good}} phonon-qt5-gstreamer imagemagick djvulibre ghostscript lib{heif,jxl,raw,rsvg,webp,wmf,xml2,zip} ocl-icd open{exr,jpeg2} wget jq
 
     #Games, etc
 sudo pacman -Syu --needed --noconfirm wine{,-gecko,-mono} lutris steam plymouth breeze-plymouth retroarch{,-assets-{glui,ozone,xmb}} gamemode lib{32-gamemode,retro-{dolphin,pcsx2,citra,melonds,duckstation}}
@@ -75,12 +73,14 @@ if [ $artix == y ]; then
 fi
 
 #Hyprland 
-#if [ $de == 1 ];then
+if [ $de == 1 ];then
+sudo pacman -Syu --needed alacritty obs-studio btop simple-scan mpv gparted gnome-{font-viewer,boxes} kdenlive bigsh0t dvgrab mediainfo noise-suppression-for-voice open{cv,timelineio} recordmydesktop rhythmbox lollypop krita okular deluge-gtk
 sudo pacman -Syu --needed --noconfirm wl-clipboard cliphist qt{5{ct,-wayland},6{ct,-wayland}} pavucontrol nemo{,-{fileroller,share}} catdoc odt2txt poppler libgsf gvfs-{mtp,afc,nfs,smb} ffmpegthumbnailer polkit-gnome imv calcurse gamescope brightnessctl udiskie gammastep swayidle hyprland xdg-desktop-portal-hyprland breeze-{icons,gtk}
 paru -S --needed rofi-lbonn-wayland-git waybar-hyprland-git hyprpicker-git swww nwg-look swaync wlr-randr grimblast swaylock-effects-git psuinfo
-sudo pacman -S --needed --noconfirm rofi-calc
-#elif [ $de == 2 ];then
-#    sudo pacman -Syu --needed plasma{,-wayland-session} kde-applications
+sudo pacman -Syu --needed --noconfirm rofi-calc
+elif [ $de == 2 ];then
+    sudo pacman -Syu --needed plasma{,-wayland-session} kde-applications
+fi
 #elif [ $de == 3 ];then
 #    sudo pacman -Syu --needed 
 
