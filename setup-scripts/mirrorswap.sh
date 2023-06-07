4606a708f265d47190cd3157e60bb23343c4f2b9
Metroscript <genericburner39@gmail.com>
2023-06-07 19:22:10 +1000

    Fixed syntax error

--- setup-scripts/mirrorswap.sh
+++ setup-scripts/mirrorswap.sh
@@ -5,7 +5,7 @@ fi
 
 #Repo config + doas base-devel packages
 if [ $artix == y ];then
-    sudo sed -i -z -e 's;\[galaxy\]\nInclude = /etc/pacman.d/mirrorlist;\[galaxy\]\nInclude = /etc/pacman.d/mirrorlist\n\n\[universe\]\nServer = https://universe.artixlinux.org/$arch\nServer = https://mirror1.artixlinux.org/universe/$arch\nServer = https://mirror.pascalpuffke.de/artix-universe/$arch\nServer = https://artixlinux.qontinuum.space/artixlinux/universe/os/$arch\nServer = https://mirror1.cl.netactuate.com/artix/universe/$arch\nServer = https://ftp.crifo.org/artix-universe/$arch\nServer = https://artix.sakamoto.pl/universe/$arch;' -i -z -e 's;\[lib32\]\n#Include = /etc/pacman.d/mirrorlist;\[lib32\]\n#Include = /etc/pacman.d/mirrorlist\n\n#Arch Repos\n\n#\[extra\]\n#Include = /etc/pacman.d/mirrorlist-arch\n\n#\[multilib-testing\]\n#Include = /etc/pacman.d/mirrorlist-arch\n\n#\[multilib\]\n#Include = /etc/pacman.d/mirrorlist-arch;' /etc/pacman.conf
+    sudo sed -i -z -e 's;\[galaxy\]\nInclude = /etc/pacman.d/mirrorlist;\[galaxy\]\nInclude = /etc/pacman.d/mirrorlist\n\n\[universe\]\nServer = https://universe.artixlinux.org/$arch\nServer = https://mirror1.artixlinux.org/universe/$arch\nServer = https://mirror.pascalpuffke.de/artix-universe/$arch\nServer = https://artixlinux.qontinuum.space/artixlinux/universe/os/$arch\nServer = https://mirror1.cl.netactuate.com/artix/universe/$arch\nServer = https://ftp.crifo.org/artix-universe/$arch\nServer = https://artix.sakamoto.pl/universe/$arch;' -i -z -e 's;\[lib32\]\n#Include = /etc/pacman.d/mirrorlist;\[lib32\]\n#Include = /etc/pacman.d/mirrorlist\n\n#Arch Repos\n\n#\[testing\]\n#Include = /etc/pacman.d/mirrorlist-arch\n\n#\[extra\]\n#Include = /etc/pacman.d/mirrorlist-arch\n\n#\[multilib-testing\]\n#Include = /etc/pacman.d/mirrorlist-arch\n\n#\[multilib\]\n#Include = /etc/pacman.d/mirrorlist-arch;' /etc/pacman.conf
     sudo pacman -Syu --needed --noconfirm artix-archlinux-support
     sudo sed -i -e "/\[lib32\]/,/Include/"'s/^#//' -i -e "/\[extra\]/,/Include/"'s/^#//' /etc/pacman.conf
     sudo pacman-key --populate
@@ -23,25 +23,25 @@ if [ $artix == y ];then
     #####################################
     ### ADD SUPPORT FOR MORE COUNTRIES ##
     #####################################
-    if grep -E Australia <<< $countr;then
+    if grep -E "Australia" <<< ${countr};then
         country=Australia
-    elif grep -E Canada <<< $countr;then
+    elif grep -E "Canada" <<< ${countr};then
         country=Canada
-    elif grep -E France <<< $countr;then
+    elif grep -E "France" <<< ${countr};then
         country=France
-    elif grep -E Germany <<< $countr;then
+    elif grep -E "Germany" <<< ${countr};then
         country=Germany
-    elif grep -E US <<< $countr;then
+    elif grep -E "US" <<< ${countr};then
         country=US
-    elif grep -E Mexico <<< $countr;then
+    elif grep -E "Mexico" <<< ${countr};then
         country=Mexico
-    elif grep -E Chile <<< $countr;then
+    elif grep -E "Chile" <<< ${countr};then
         country=Chile
-    elif grep -E Japan <<< $countr;then
+    elif grep -E "Japan" <<< ${countr};then
         country=Japan
-    elif grep -E China <<< $countr;then
+    elif grep -E "China" <<< ${countr};then
         country=China
-    elif grep -E America <<< $countr;then
+    elif grep -E "America" <<< ${countr};then
         country=America
     fi
     ###################################
