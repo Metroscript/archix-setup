9a33c01f22abf537a14bac2125f9ee67096c00a0
Metroscript <122935820+Metroscript@users.noreply.github.com>
2023-08-07 10:00:24 +1000

    Changed Supend-then-Hibernate to hibernate after one hour

--- setup-scripts/mirrorswap.sh
+++ setup-scripts/mirrorswap.sh
@@ -49,7 +49,7 @@ if [ $swap -gt 0 ];then
         if ! grep 'sleep.conf.d' <<< $(ls $sdir);then
             sudo mkdir ${sdir}sleep.conf.d
         fi
-        sudo sh -c "echo 'HibernateDelaySec=180min' > ${sdir}sleep.conf.d/99-Hibernate-Sec.conf"
+        sudo sh -c "echo 'HibernateDelaySec=60min' > ${sdir}sleep.conf.d/99-Hibernate-Sec.conf"
         sudo sh -c "echo -e '[Sleep]\nHibernateMode=shutdown' > ${sdir}sleep.conf.d/99-Hibernate-Mode.conf"
         if [ $de == 1 ];then
             sed -i "s/ctl suspend/ctl suspend-then-hibernate \|\| $(if [ "$artix" == y ];then echo loginctl;else echo systemctl;fi) suspend/" ${repo}dotfiles/hypr-rice/wlogout/layout
