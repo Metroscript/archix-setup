6df3708f32719d94a1f6cf263184dabd699478e6
Metroscript <122935820+Metroscript@users.noreply.github.com>
2024-01-01 18:07:44 +1100

    Get the value of  regardless of reflector status & make installation of virtualisation tools optional

---
+++ scripts/mirrorswap.sh
@@ -0,0 +1,85 @@
+#Repo config + doas base-devel packages
+if [ "$artix" == y ];then
+    sudo sed -i -z -e 's,\[galaxy\]\nInclude = /etc/pacman.d/mirrorlist,\[galaxy\]\nInclude = /etc/pacman.d/mirrorlist\n\n\[universe\]\nServer = https://universe.artixlinux.org/$arch\nServer = https://mirror1.artixlinux.org/universe/$arch\nServer = https://mirror.pascalpuffke.de/artix-universe/$arch\nServer = https://mirrors.qontinuum.space/artixlinux-universe/$arch\nServer = https://mirror1.cl.netactuate.com/artix/universe/$arch\nServer = https://ftp.crifo.org/artix-universe/$arch\nServer = https://artix.sakamoto.pl/universe/$arch,' -i -z -e 's,\[lib32\]\n#Include = /etc/pacman.d/mirrorlist,\[lib32\]\n#Include = /etc/pacman.d/mirrorlist\n\n#Arch Repos\n\n#\[extra-testing\]\n#Include = /etc/pacman.d/mirrorlist-arch\n\n#\[extra\]\n#Include = /etc/pacman.d/mirrorlist-arch\n\n#\[multilib-testing\]\n#Include = /etc/pacman.d/mirrorlist-arch\n\n#\[multilib\]\n#Include = /etc/pacman.d/mirrorlist-arch,' /etc/pacman.conf
+    sudo pacman -Syu --needed --noconfirm artix-archlinux-support
+    sudo sed -i -e "/\[lib32\]/,/Include/"'s/^#//' -i -e "/\[extra\]/,/Include/"'s/^#//' /etc/pacman.conf
+    sudo pacman-key --populate
+fi
+    sudo sed -i -e 's/#Color/Color/' -i -e '/Color/a ILoveCandy' -i -e 's/#Verbose/Verbose/' -i -e 's/#Parallel/Parallel/' -i -e "/\[multilib\]/,/Include/"'s/^#//' /etc/pacman.conf
+sudo pacman -Syu --needed --noconfirm base-devel
+if [ "$suas" == y ];then
+    sudo pacman -R --noconfirm base-devel
+    sudo pacman -Rns --noconfirm sudo
+fi
+tz=$(curl https://ipapi.co/timezone)
+if [ $reflect == y ];then
+    sudo pacman -Syu --needed --noconfirm reflector rsync neovim
+    country=$(curl https://ipapi.co/timezone | cut -d/ -f1)
+    if [ "$artix" == y ];then
+        sudo mv /etc/pacman.d/mirrorlist /etc/pacman.d/mirrorlist.pacnew
+        sudo sh -c "rankmirrors /etc/pacman.d/mirrorlist.pacnew > /etc/pacman.d/mirrorlist"
+        sudo reflector --save /etc/pacman.d/mirrorlist-arch --sort rate -c $country -p https;else
+        sudo reflector --save /etc/pacman.d/mirrorlist --sort rate -c $country -p https
+    fi
+    sudo sh -c "echo 'Server = https://geo.mirror.pkgbuild.com/$repo/os/$arch' >> /etc/pacman.d/mirrorlist"
+    sudo nvim /etc/pacman.d/mirrorlist
+fi
+sudo pacman -Syu --noconfirm --needed pacman-contrib pkgfile
+sudo pkgfile -uz "zstd --ultra -22 -T0"
+
+if [ "$btrfs" == y ];then
+   sudo pacman -Syu --needed --noconfirm grub-btrfs btrfs-progs
+fi
+
+#Make Swapfile
+if [ "$swap" -gt 0 ];then
+    if [ "$btrfs" == y ];then
+        cd /
+        sudo btrfs subvolume create ${swapvol}
+        sudo btrfs filesystem mkswapfile --size ${swap}G --uuid clear ${swapvol}/swapfile
+        sudo swapon ${swapvol}/swapfile
+        sudo cp /etc/fstab /etc/fstab.bak
+        sudo sh -c "echo '${swapvol}/swapfile none swap defaults 0 0' >> /etc/fstab"
+        cd;else
+        sudo dd if=/dev/zero of=/swapfile bs=1M count=$((${swap}*1024)) status=progress
+        sudo chmod 600 /swapfile
+        sudo mkswap -U clear /swapfile
+        sudo cp /etc/fstab /etc/fstab.bak
+        sudo sh -c "echo '/swapfile none swap defaults 0 0' >> /etc/fstab"
+        sudo mount -a
+        sudo swapon -a
+    fi
+    if [ $res == y ];then
+        sudo sed -i "s.quiet.resume=$(cat /etc/fstab | grep '/ ' | cut -d/ -f1 | awk '{$1=$1};1') resume_offset=$(if [ "$btrfs" == y ];then sudo btrfs inspect-internal map-swapfile -r /${swapvol}/swapfile;else sudo filefrag -v /swapfile | awk '$1=="0:" {print substr($4, 1, length($4)-2)}';fi) quiet." $bootdir
+    fi
+        if [ $img == mkinit ];then
+            sudo sed -i 's/filesystems/filesystems resume/' /etc/mkinitcpio.conf
+            sudo mkinitcpio -P
+        ###############################################
+        ####### OTHER INITRAMFS GEN SUPPORT ###########
+        ###############################################
+        #elif [ $img == dracut ];then
+        #    sudo sh -c 'echo add_dracutmodules+=\" resume \" > /etc/dracut.conf.d/resume.conf'
+        #    sudo dracut-rebuild #;else
+            #BOOSTER STUFF HERE
+            #sudo booster build
+        fi
+        if ! grep 'sleep.conf.d' <<< $(ls $sdir);then
+            sudo mkdir ${sdir}sleep.conf.d
+        fi
+        sudo sh -c "echo -e '[Sleep]\nHibernateDelaySec=60min\nHibernateMode=shutdown' > ${sdir}sleep.conf.d/99-Hibernate-Mode-and-Delay.conf"
+        if [ $de == 1 ];then
+            sed -i "s/ctl suspend/ctl suspend-then-hibernate \|\| $(if [ "$artix" == y ];then echo loginctl;else echo systemctl;fi) suspend/" ${repo}dotfiles/hypr-rice/wlogout/layout
+        fi
+fi
+
+if [ "$zram" -gt 0 ];then
+    sudo sh -c "echo 'zram' > /etc/modules-load.d/zram.conf"
+    sudo sh -c "echo 'options zram num_devices=1' > /etc/modprobe.d/zram.conf"
+    echo ACTION==\"add\", KERNEL==\"zram0\", ATTR{comp_algorithm}=\"${zramcomp}\", ATTR{disksize}=\"${zram}G\", RUN=\"/usr/bin/mkswap -U clear /dev/zram0\" | sudo tee /etc/udev/rules.d/99-zram.rules
+    sudo sed -i 's;quiet;zswap.enabled=0 quiet;' /etc/default/grub
+    sudo sh -c "echo '/dev/zram0 none swap defaults,pri=100 0 0' >> /etc/fstab"
+fi
+if grep nvme <<< $(lsblk) && ! grep nvme_load <<< $(cat $bootdir);then
+    sudo sed -i 's/quiet/nvme_load=YES quiet/' $bootdir
+fi
---
+++ scripts/questions.sh
@@ -0,0 +1,295 @@
+if grep mkinitcpio <<< $(pacman -Q);then
+    img=mkinit
+elif grep dracut <<< $(pacman -Q);then
+    img=dracut;else
+    img=booster
+fi
+
+###################################################################################################
+########### ADD SUPPORT FOR OTHER INITRAMFs GENERATORs? ###########################################
+###################################################################################################
+#if ( [ $img == dracut ] && ! grep 'hostonly="true"' /etc/dracut.conf.d/* ) || ( [ $img == booster ] && ! grep 'universal: false' /etc/booster.yaml && ! grep 'universal: true' /etc/booster.yaml );then
+#    printf "Enable hostonly initramfs generation? (Speeds up generation by excluding modules your system doesn't need) [y/n]: "
+#    read hostonly
+#    until [ $hostonly == y ] || [ $hostonly == n ];do
+#        printf "Enable hostonly initramfs generation? (Speeds up generation by excluding modules your system doesn't need) [y/n]: "
+#        read hostonly
+#    done
+#    if [ $hostonly == y ];then
+#        if [ $img == dracut ];then
+#            sudo sh -c 'echo hostonly=\"yes\" > /etc/dracut.conf.d/hostonly.conf'
+#            sudo dracut-rebuild #;else
+            #sudo sh -c 'echo "universal: false" >> /etc/booster.yaml'
+            #sudo booster build
+#        fi
+#    fi
+#fi
+
+if grep Artix <<< $(cat /etc/issue);then
+    artix=y
+    sdir=/etc/elogind/
+    sed -i -e 's/#exec-once/exec-once/' -i -e '/--systemd/d' -i -e '/systemctl/d' ${repo}dotfiles/hypr-rice/hypr/hyprland.conf
+    sed -i -e 's/action" : "reboot/action" : "loginctl reboot/' -i -e 's/poweroff/loginctl poweroff/' -i -e 's/action" : "suspend/action" : "loginctl suspend/' ${repo}dotfiles/hypr-rice/wlogout/layout
+    if grep openrc <<< $(pacman -Q);then
+        init=openrc
+    elif grep runit <<< $(pacman -Q);then
+        init=runit
+    elif grep s6-base <<< $(pacman -Q);then
+        init=s6
+    elif grep dinit <<< $(pacman -Q);then
+        init=dinit
+    fi;else
+    sdir=/etc/systemd/
+fi
+
+bootdir=/etc/default/grub
+
+if grep btrfs <<< $(lsblk -oFSTYPE);then
+    btrfs=y
+fi
+
+if grep opendoas <<< $(pacman -Q);then
+    suas=y
+    alias sudo='doas'
+    sed -i "/stuff/a alias sudo='doas'" ${repo}dotfiles/bashrc
+    echo "-----------------------------------------------------------------------------------------"
+    echo -e "SudoLoop is enabled on paru, when tweaking doas.conf, put 'permit persist :wheel cmd true' so SudoLoop works\nYou should make any changes & run 'chmod 0400 /etc/doas.conf' as root after install"
+    echo "-----------------------------------------------------------------------------------------"
+    sleep 5
+fi
+
+############################################################################
+########### ADD SUPPORT FOR OTHER GUIs? ####################################
+############################################################################
+echo "Which DE/WM would you like to install? 1.Hyprland or 2.KDE Plasma"
+printf "[1/2]: "
+read de
+until [ $de == 1 ] || [ $de == 2 ];do
+    echo "Please try again. Which DE/WM would you like to install? 1.Hyprland or 2.KDE Plasma"
+    printf "[1/2]: "
+    read de
+done
+if [ $de == 1 ] || [ $de == 2 ];then
+    dm=sddm
+fi
+############################################################################
+####################### END OF PROBLEM AREA ################################
+############################################################################
+
+printf "Use precompiled AUR binaries where availiable? [y/n]: "
+read bin
+until [ $bin == y ] || [ $bin == n ];do
+    echo "Sorry, please try again."
+    printf "Use precompiled binaries where availiable? [y/n]: "
+    read bin
+done
+
+printf "Enable compile optimisations such as multithreading & native binaries to makepkg.conf? [y/n]: "
+read opt
+until [ $opt == y ] || [ $opt == n ];do
+    echo "Sorry, please try again."
+    printf "Enable compile optimisations such as multithreading & native binaries to makepkg.conf? [y/n]: "
+    read opt
+done
+
+printf "Install multithreaded drop-ins for gzip & bzip2? [y/n]: "
+read mtdi
+until [ $mtdi == y ] || [ $mtdi == n ];do
+    echo "Sorry, please try again."
+    printf "Install multithreaded drop-ins for gzip & bzip2? [y/n]: "
+    read mtdi
+done
+
+printf "Install Virtualisation Software? [y/n]: "
+read vir
+until [ $vir == y ] || [ $vir == n ];do
+    echo "Sorry, please try again"
+    printf "Install Virtualisation Software? [y/n]: "
+    read vir
+done
+if [ $vir == y ];then
+    echo "Install 1.Qemu, 2.Virtualbox or 3.Both?"
+    printf " [1/2/3]: "
+    read virt
+    until [ "$virt" == 1 ] || [ "$virt" == 2 ] || [ "$virt" == 3 ];do
+        echo "Sorry, please try again"
+        echo "Install 1.Qemu, 2.Virtualbox or 3.Both?"
+        printf " [1/2/3]: "
+        read virt
+    done
+fi
+printf "Install Emulation & Steam/WINE support? [y/n]: "
+read gayms
+until [ $gayms == y ] || [ $gayms == n ];do
+    echo "Sorry, please try again"
+    printf "Install Emulation & Steam/WINE support? [y/n]: "
+    read gayms
+done
+if [ $gayms == y ];then
+    printf "Install Vinegar? (A WINE wrapper for Roblox) [y/n]: "
+    read rlx
+    until [ $rlx == y ] || [ $rlx == n ];do
+        echo "Sorry, please try again"
+        printf "Install Vinegar? (A WINE wrapper for Roblox) [y/n]: "
+        read rlx
+    done
+    printf "Install Prismlauncher? (A custom Minecraft launcher with mod support) [y/n]: "
+    read min
+    until [ $min == y ] || [ $min == n ];do
+        echo "Sorry, please try again."
+        printf "Install Prismlauncher? (A custom Minecraft launcher with mod support) [y/n]: "
+        read min
+    done
+fi
+echo "What shell would you like to use? (Use BASH if unsure) 1.BASH 2.FISH"
+printf "[1/2]: "
+read shel
+until [ $shel == 1 ] || [ $shel == 2 ];do
+    echo "What shell would you like to use? (Use BASH if unsure) 1.BASH 2.FISH"
+    printf "[1/2]: "
+    read shel
+done
+if [ $shel == 1 ];then
+    shell=bash;else
+    shell=fish
+fi
+echo "What cron would you like? (If unsure, choose Cronie) 1.Cronie or 2.Fcron"
+printf "[1/2]: "
+read crock
+until [ $crock == 1 ] || [ $crock == 2 ];do
+    echo "What cron would you like? (If unsure, choose Cronie) 1.Cronie or 2.Fcron"
+    printf "[1/2]: "
+    read crock
+done
+if [ $crock == 1 ];then
+    cron=cronie;else
+    cron=fcron
+fi
+printf "Install reflector to find closer package mirrors? [y/n]: "
+read reflect
+until [ $reflect == y ] || [ $reflect == n ];do
+    printf "Install reflector to find close package mirrors? [y/n]: "
+    read reflect
+done
+printf "Install Plymouth? (Adds boot splash screen) [y/n]: "
+read ply
+until [ $ply == y ] || [ $ply == n ];do
+    printf "Install Plymouth? (Adds boot splash screen) [y/n]: "
+    read ply
+done
+if [ $ply == y ];then
+    echo 'Select Plymouth Theme: spinner, bgrt, breeze, breeze-text, fade-in, glow, solar, spininfinity, spinner, text, tribar, script, details.'
+    printf "Type desired theme name: "
+    read plytheme
+    until [ "$plytheme" == spinner ] || [ "$plytheme" == bgrt ] || [ "$plytheme" == breeze ] || [ "$plytheme" == breeze-text ] || [ "$plytheme" == fade-in ] || [ "$plytheme" == glow ] || [ "$plytheme" == solar ] ||  [ "$plytheme" == spininfinity ] || [ "$plytheme" == text ] || [ "$plytheme" == tribar ] || [ "$plytheme" == script ] || [ "$plytheme" == details ];do
+        echo 'Sorry, please try again.'
+        echo 'Select Plymouth Theme: spinner, bgrt, breeze, breeze-text, fade-in, glow, solar, spininfinity, spinner, text, tribar, script, details.'
+        printf "Type desired theme name: "
+        read plytheme
+    done
+    if [ "$plytheme" == bgrt ];then
+        printf "Remove distro watermark to leave only BIOS vender logo? [y/n] "
+        read vr
+        until [ "$vr" == y ] || [ "$vr" == n ];do
+            echo "Sorry, please try again."
+            printf "Remove distro watermark to leave only BIOS vender logo? [y/n] "
+            read vr
+        done
+    fi
+fi
+printf "Install MakeMKV? (A DVD/Bluray ripper) [y/n]: "
+read makemkv
+until [ $makemkv == y ] || [ $makemkv == n ];do
+    printf "Install MakeMKV? (A DVD/Bluray ripper) [y/n]: "
+    read makemkv
+done
+printf "Install OpenRGB? (RGB management software) [y/n]: "
+read rgb
+until [ $rgb == y ] || [ $rgb == n ];do
+    printf "Install OpenRGB? (RGB management software) [y/n]: "
+    read rgb
+done
+if [ $rgb == y ];then
+    printf "Are you planning to control motherboard or RAM LEDs? [y/n]: "
+    read rgsmb
+    until [ $rgsmb == y ] || [ $rgsmb == n ];do
+        printf "Are you planning to control motherboard or RAM LEDs? [y/n]: "
+        read rgsmb
+    done
+fi
+if [ $img == mkinit ];then
+    printf "Install mkinitcpio-firmware (Removes missing firmware warnings when generating initramfs) [y/n]: "
+    read mkfirm
+    until [ $mkfirm == y ] || [ $mkfirm == n ];do
+        echo "Sorry, please try again."
+        printf "Install mkinitcpio-firmware (Removes missing firmware warnings when generating initramfs) [y/n]: "
+        read mkfirm
+    done
+fi
+printf "Add installed kernels & firmware to IgnorePkg? [y/n]: "
+read kignore
+until [ $kignore == y ] || [ $kignore == n ];do
+    echo "Sorry, please try again."
+    printf "Add installed kernels & firmware to IgnorePkg? [y/n]: "
+    read kignore
+done
+
+if ! grep Size <<< $(swapon -s);then
+    echo "Swapfile size in GiB. Matching RAM size OR RAM x 1.5 sized swap is usually a good choice for hibernation. Put '0' for no swapfile."
+    printf "Size of swapfile in GiB: "
+    read swap
+    until [ $swap -ge 0 ];do
+        echo "Sorry, please try again."
+        echo "Swapfile size in GiB. Matching RAM size OR RAM x 1.5 sized swap is usually a good choice for hibernation. Put '0' for no swapfile."
+        printf "Size of swapfile in GiB: "
+        read swap
+    done
+    if [ $swap -gt 0 ];then
+        if [ "$btrfs" == y ];then
+            printf "What would you like your swap subvolume to be called?: "
+            read swapvol
+            echo "Are you sure you want to call your swap subvolume \"${swapvol}\"?"
+            printf "[y/n]: "
+            read swapvolconf
+            until [ "$swapvolconf" == y ];do
+                printf "What would you like your swap subvolume to be called?: "
+                read swapvol
+                echo "Are you sure you want to call your swap subvolume \"${swapvol}\"?"
+                printf "[y/n]: "
+                read swapvolconf
+            done
+        fi
+        printf "Enable suspend to & resume from disk support? [y/n]: "
+        read res
+        until [ $res == y ] || [ $res == n ];do 
+            printf "Enable suspend to & resume from disk support? [y/n]: "
+            read res
+        done
+    fi
+fi
+if ! grep zram <<< $(lsblk);then
+    echo "Would you like to use zram? (Compressed RAM; faster than standard swap) Please input the size in GiB of uncompressed data zram should have. Half of RAM is usually good. Put 0 for no zram"
+    printf "Size of zram in GiB: "
+    read zram
+    until [ "$zram" -ge 0 ];do
+        echo "Sorry, please try again."
+        echo "Would you like to use zram? (Compressed RAM; faster than standard swap) Please input the size in GiB of uncompressed data zram should have. Half of RAM is usually good. Put 0 for no zram"
+        printf "Size of zram in GiB: "
+        read zram
+    done
+    if [ "$zram" -gt 0 ];then
+        echo -e "Please select the compression algorithm for zram. 1. LZ4, 2. ZSTD.\nLZ4 is faster, but less effective. ZSTD is slower, but more effective at compression"
+        printf "1 OR 2: "
+        read zramc
+        until [ "$zramc" == 1 ] || [ "$zramc" == 2 ];do
+            echo "Sorry, please try again."
+            echo -e "Please select the compression algorithm for zram. 1. LZ4, 2. ZSTD.\nLZ4 is faster, but less effective. ZSTD is slower, but more effective at compression"
+            printf "1 OR 2: "
+            read zramc
+        done
+        if [ "$zramc" == 1 ];then
+            zramcomp=lz4;else
+            zramcomp=zstd
+        fi
+    fi
+fi
